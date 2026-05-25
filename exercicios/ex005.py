9eb6ba08fa644264b212daa49c42166808e7276f
Aline Medeiros <workalinemedeiros@gmail.com>
2026-05-25 12:30:14 -0300

    Novas aulas operadores aritméticos

---
+++ exercicios/ex005.py
@@ -0,0 +1,4 @@
+#desafio da aula 7 - Ler um número inteiro e mostrar seu sucessor e antecessor.#
+
+n1 = int(input('Digite um número:'))
+print('O antecesssor e sucessor do número digitado é {} e {}'.format(n1-1, n1+1))
