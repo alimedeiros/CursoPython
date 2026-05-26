c3fdab6a01abc4f9f580adea222db69a1f3110c3
Aline Medeiros <workalinemedeiros@gmail.com>
2026-05-26 11:35:21 -0300

    Exercício da aula 7

---
+++ exercicios/ex006.py
@@ -0,0 +1,4 @@
+#desafio da aula 7 - Mostre o dobro, triplo e raiz quadrade de um número#
+
+n = int(input('Digite um número:'))
+print('O dobro, triplo e raiz quadrada deste número são respectivamente {}, {} e {}'.format(n*2, n*3, n**(1/2)))
